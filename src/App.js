f0bd786d020c00c560738c3d70d3d9ffb4f7844b
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-08-17 21:50:05 +0200

    Styling Menu Items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import HomePage from './components/HomePage/HomePage';
+import HomePage from './pages/homepage/HomePage';
 
 function App() {
   return (
---
+++ src/components/menuItem/MenuItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './MenuItem.styles.scss';
+
+const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    //                                                          add background image and imageUrl
+    <div className={`${size} menu-item`}>
+      <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>Shop Now</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
