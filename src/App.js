0a91920d6e42d516260f64a138dfc4fdef133347
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-08-19 22:40:55 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.scss';
 import HomePage from './pages/homepage/HomePage';
 
+import { Router, Route, browserHistory, Switch } from 'react-router';
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menuItem/MenuItem.js
+++ src/components/menuItem/MenuItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './MenuItem.styles.scss';
+import { withRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    //                                                          add background image and imageUrl
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+      {/*  add background image and imageUrl */}
       <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className='content'>
         <h1 className='title'>{title.toUpperCase()}</h1>
@@ -14,4 +15,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
