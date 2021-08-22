e2b173b8a35b12282137bb4919ec11f1af484db9
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-08-22 21:24:58 +0200

    Sign Up With Email and Password

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './pages/shop/ShopPage';
 import Header from './components/header/Header';
 import SignInAndUpPage from './pages/signInAndUpPage/SignInAndUpPage';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor(props) {
@@ -20,9 +20,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+
+          console.log(this.state);
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
--- src/pages/signInAndUpPage/SignInAndUpPage.js
+++ src/pages/signInAndUpPage/SignInAndUpPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './SignInAndUpPage.styles.scss';
 import SignIn from '../../components/signIn/SignIn';
+import SignUp from '../../components/signUp/SignUp';
 
 const signInAndUpPage = () => {
   return (
     <div className='sign-in-and-sign-up'>
       <SignIn />
+      <SignUp />
     </div>
   );
 };
